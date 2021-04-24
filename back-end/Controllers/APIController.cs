6c5def53ff1aabb5b1fffd80d07d508c6dcd9bf7
Dung Pham <dungphamdev@gmail.com>
2021-04-24 14:06:19 +0700

    update api

--- back-end/Controllers/APIController.cs
+++ back-end/Controllers/APIController.cs
@@ -1,6 +1,73 @@
-﻿using Microsoft.AspNetCore.Mvc; using System; using System.Linq; using WebApi.Entities.Models; using WebApi.Models; using WebApi.Services; using WebApi.Messages.Requests.users; using WebApi.Messages.Response.users; using System.Collections.Generic; using System.Net;  namespace WebApi.Controllers {     [ApiController]     //[Route("[controller]")]     [Route("api")]     public class APIController : ControllerBase     {         private IUserService _userService;         private DB_CloudKitchenContext context;          public APIController(IUserService userService, DB_CloudKitchenContext db)         {             _userService = userService;             context = db;         }          [Route("login")]         [HttpPost]         public LoginResponse Login([FromBody] LoginRequest request)         {             try             {                 var isExist = context.Customers.FirstOrDefault(f => f.Username == request.Username && f.Password == request.Password && f.Active == true);                  if (isExist != null)                 {                     return new LoginResponse                     {                         LoginStatus = true,                         Customer = isExist,                         StatusCode = (int)HttpStatusCode.OK                     };                 }                 return new LoginResponse                 {                     LoginStatus = false,                     StatusCode = (int)HttpStatusCode.BadRequest                 };             }             catch (Exception e)             {                 return new LoginResponse                 {                     LoginStatus = false,                     StatusCode = (int)HttpStatusCode.BadRequest                 };             }         }          [Route("customer/create")]         [HttpPost]         public CreateCustomerResponse CreateCustomer([FromBody] CreateCustomerRequest request)         {             try             {                 string Username = context.Customers.FirstOrDefault(f => f.Username == request.customer.Username)?.Username;                                           
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+using WebApi.Entities.Models;
+using WebApi.Models;
+using WebApi.Services;
+using WebApi.Messages.Requests.users;
+using WebApi.Messages.Response.users;
+using System.Collections.Generic;
+using System.Net;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    //[Route("[controller]")]
+    [Route("api")]
+    public class APIController : ControllerBase
+    {
+        private IUserService _userService;
+        private DB_CloudKitchenContext context;
+
+        public APIController(IUserService userService, DB_CloudKitchenContext db)
+        {
+            _userService = userService;
+            context = db;
+        }
+
+        [Route("login")]
+        [HttpPost]
+        public LoginResponse Login([FromBody] LoginRequest request)
+        {
+            try
+            {
+                var isExist = context.Customers.FirstOrDefault(f => f.Username == request.Username && f.Password == request.Password && f.Active == true);
+
+                if (isExist != null)
+                {
+                    return new LoginResponse
+                    {
+                        LoginStatus = true,
+                        Customer = isExist,
+                        StatusCode = (int)HttpStatusCode.OK
+                    };
+                }
+                return new LoginResponse
+                {
+                    LoginStatus = false,
+                    StatusCode = (int)HttpStatusCode.BadRequest
+                };
+            }
+            catch (Exception e)
+            {
+                return new LoginResponse
+                {
+                    LoginStatus = false,
+                    StatusCode = (int)HttpStatusCode.BadRequest
+                };
+            }
+        }
+
+        [Route("customer/create")]
+        [HttpPost]
+        public CreateCustomerResponse CreateCustomer([FromBody] CreateCustomerRequest request)
+        {
+            try
+            {
+                string Username = context.Customers.FirstOrDefault(f => f.Username == request.customer.Username)?.Username;                                           
                 if (Username?.Length == 0 || Username == null)
-                {                     var newCustomer = new Customer
+                {
+                    var newCustomer = new Customer
                     {
                         CustomerName = request.customer?.CustomerName ?? "",
                         CustomerEmailId = request.customer?.CustomerEmailId ?? "",
@@ -19,9 +86,28 @@
                     {
                         StatusCode = (int)HttpStatusCode.BadRequest
                     };
-                }                                 return new CreateCustomerResponse                 {                     StatusCode = (int)HttpStatusCode.OK                 };             }             catch (Exception e)             {                 return new CreateCustomerResponse                 {                     StatusCode = (int)HttpStatusCode.BadRequest                 };             }         }          [Route("customer/update")]
+                }                
+                return new CreateCustomerResponse
+                {
+                    StatusCode = (int)HttpStatusCode.OK
+                };
+            }
+            catch (Exception e)
+            {
+                return new CreateCustomerResponse
+                {
+                    StatusCode = (int)HttpStatusCode.BadRequest
+                };
+            }
+        }
+
+        [Route("customer/update")]
         [HttpPost]
-        public UpdateCustomerResponse UpdateCustomer([FromBody] UpdateCustomerRequest request)         {             try             {                 var customer = context.Customers.FirstOrDefault(f => f.CustomerId == request.customer.CustomerId && f.Active == true);
+        public UpdateCustomerResponse UpdateCustomer([FromBody] UpdateCustomerRequest request)
+        {
+            try
+            {
+                var customer = context.Customers.FirstOrDefault(f => f.CustomerId == request.customer.CustomerId && f.Active == true);
                 if (customer != null)
                 {
                     customer.CustomerName = request.customer?.CustomerName ?? "";
@@ -39,10 +125,27 @@
                         StatusCode = (int)HttpStatusCode.BadRequest
                     };
                 }
-                return new UpdateCustomerResponse                 {                     StatusCode = (int)HttpStatusCode.OK                 };             }             catch (Exception e)             {                 return new UpdateCustomerResponse                 {                     StatusCode = (int)HttpStatusCode.BadRequest                 };             }         }
+                return new UpdateCustomerResponse
+                {
+                    StatusCode = (int)HttpStatusCode.OK
+                };
+            }
+            catch (Exception e)
+            {
+                return new UpdateCustomerResponse
+                {
+                    StatusCode = (int)HttpStatusCode.BadRequest
+                };
+            }
+        }
 
         [Route("customer/delete")]
-        [HttpPost]         public DeleteCustomerResponse DeleteCustomer([FromBody] DeleteCustomerRequest request)         {             try             {                 var customer = context.Customers.FirstOrDefault(f => f.CustomerId == request.customer.CustomerId && f.Active == true);
+        [HttpPost]
+        public DeleteCustomerResponse DeleteCustomer([FromBody] DeleteCustomerRequest request)
+        {
+            try
+            {
+                var customer = context.Customers.FirstOrDefault(f => f.CustomerId == request.customer.CustomerId && f.Active == true);
                 if (customer != null)
                 {
                     customer.Active = false;
@@ -57,7 +160,21 @@
                         StatusCode = (int)HttpStatusCode.BadRequest
                     };
                 }
-                return new DeleteCustomerResponse                 {                     StatusCode = (int)HttpStatusCode.OK                 };             }             catch (Exception e)             {                 return new DeleteCustomerResponse                 {                     StatusCode = (int)HttpStatusCode.BadRequest                 };             }         }          //[Route("customer/list")]
+                return new DeleteCustomerResponse
+                {
+                    StatusCode = (int)HttpStatusCode.OK
+                };
+            }
+            catch (Exception e)
+            {
+                return new DeleteCustomerResponse
+                {
+                    StatusCode = (int)HttpStatusCode.BadRequest
+                };
+            }
+        }
+
+        //[Route("customer/list")]
         //[HttpPost]
         //public GetListCustomerResponse GetAlUsers()
         //{
@@ -80,4 +197,141 @@
         //    }
         //}
 
-        //[Route("users/create")]         //[HttpPost]         //public CreateUserResponse CreateUser([FromBody] CreateUserRequest request)         //{         //    try         //    {         //        var newUser = new User         //        {         //            UserId = Guid.NewGuid(),         //            FirstName = request.user?.FirstName ?? "",         //            LastName = request.user?.LastName ?? "",         //            Address = request.user?.LastName ?? "",         //            City = request.user?.City ?? "",         //        };          //        context.Users.Add(newUser);         //        context.SaveChanges();          //        return new CreateUserResponse         //        {         //            StatusCode = (int)HttpStatusCode.OK         //        };         //    }         //    catch (Exception e)         //    {         //        return new CreateUserResponse         //        {         //            StatusCode = (int)HttpStatusCode.BadRequest         //        };         //    }         //}          //[Route("users/update")]         //[HttpPost]         //public UpdateUserResponse UpdateUser([FromBody] UpdateUserRequest request)         //{         //    try         //    {         //        var user = context.Users.FirstOrDefault(f => f.UserId == request.user.UserId);          //        if (user != null)         //        {         //            user.FirstName = request.user?.FirstName ?? "";         //            user.LastName = request.user?.LastName ?? "";         //            user.Address = request.user?.Address ?? "";         //            user.City = request.user?.City ?? "";          //            context.Users.Update(user);         //            context.SaveChanges();         //        }          //        return new UpdateUserResponse         //        {         //            StatusCode = (int)HttpStatusCode.OK         //        };         //    }         //    catch (Exception e)         //    {         //        return new UpdateUserResponse         //        {         //            StatusCode = (int)HttpStatusCode.BadRequest         //        };         //    }         //}          //[Route("users/delete")]         //[HttpPost]         //public DeleteUserResponse DeleteUser([FromBody] DeleteUserRequest request)         //{         //    try         //    {         //        var user = context.Users.FirstOrDefault(f => f.UserId == request.UserId);          //        if (user != null)         //        {         //            context.Users.Remove(user);         //            context.SaveChanges();         //        }          //        return new DeleteUserResponse         //        {         //            StatusCode = (int)HttpStatusCode.OK         //        };         //    }         //    catch (Exception e)         //    {         //        return new DeleteUserResponse         //        {         //            StatusCode = (int)HttpStatusCode.BadRequest         //        };         //    }         //}          //[Route("authenticate")]         //[HttpPost]         ////[HttpPost("authenticate")]         //public IActionResult Authenticate(AuthenticateRequest model)         //{         //    var response = _userService.Authenticate(model);          //    Console.WriteLine("Hello World!");          //    if (response == null)         //        return BadRequest(new { message = "Username or password is incorrect" });          //    return Ok(response);         //}          ////[Authorize]         //[HttpGet]         //public IActionResult GetAll()         //{         //    var users = _userService.GetAll();         //    return Ok(users);         //}          //[HttpGet("persons")]         //public IActionResult GetAllPerson()         //{         //    //var users = _userService.GetAll();         //    var person = context.Persons.ToList();         //    return Ok(person);         //}          //[HttpPost("persons/getPersonById")]         //public IActionResult GetPersonById(getPersonByIdParameterModel model)         //{         //    //var users = _userService.GetAll();         //    var person = context.Persons.FirstOrDefault(f => f.Id == model.id);         //    return Ok(person);         //}     }      public class getPersonByIdParameterModel     {         public int id { get; set; }     } } 
+        //[Route("users/create")]
+        //[HttpPost]
+        //public CreateUserResponse CreateUser([FromBody] CreateUserRequest request)
+        //{
+        //    try
+        //    {
+        //        var newUser = new User
+        //        {
+        //            UserId = Guid.NewGuid(),
+        //            FirstName = request.user?.FirstName ?? "",
+        //            LastName = request.user?.LastName ?? "",
+        //            Address = request.user?.LastName ?? "",
+        //            City = request.user?.City ?? "",
+        //        };
+
+        //        context.Users.Add(newUser);
+        //        context.SaveChanges();
+
+        //        return new CreateUserResponse
+        //        {
+        //            StatusCode = (int)HttpStatusCode.OK
+        //        };
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        return new CreateUserResponse
+        //        {
+        //            StatusCode = (int)HttpStatusCode.BadRequest
+        //        };
+        //    }
+        //}
+
+        //[Route("users/update")]
+        //[HttpPost]
+        //public UpdateUserResponse UpdateUser([FromBody] UpdateUserRequest request)
+        //{
+        //    try
+        //    {
+        //        var user = context.Users.FirstOrDefault(f => f.UserId == request.user.UserId);
+
+        //        if (user != null)
+        //        {
+        //            user.FirstName = request.user?.FirstName ?? "";
+        //            user.LastName = request.user?.LastName ?? "";
+        //            user.Address = request.user?.Address ?? "";
+        //            user.City = request.user?.City ?? "";
+
+        //            context.Users.Update(user);
+        //            context.SaveChanges();
+        //        }
+
+        //        return new UpdateUserResponse
+        //        {
+        //            StatusCode = (int)HttpStatusCode.OK
+        //        };
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        return new UpdateUserResponse
+        //        {
+        //            StatusCode = (int)HttpStatusCode.BadRequest
+        //        };
+        //    }
+        //}
+
+        //[Route("users/delete")]
+        //[HttpPost]
+        //public DeleteUserResponse DeleteUser([FromBody] DeleteUserRequest request)
+        //{
+        //    try
+        //    {
+        //        var user = context.Users.FirstOrDefault(f => f.UserId == request.UserId);
+
+        //        if (user != null)
+        //        {
+        //            context.Users.Remove(user);
+        //            context.SaveChanges();
+        //        }
+
+        //        return new DeleteUserResponse
+        //        {
+        //            StatusCode = (int)HttpStatusCode.OK
+        //        };
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        return new DeleteUserResponse
+        //        {
+        //            StatusCode = (int)HttpStatusCode.BadRequest
+        //        };
+        //    }
+        //}
+
+        //[Route("authenticate")]
+        //[HttpPost]
+        ////[HttpPost("authenticate")]
+        //public IActionResult Authenticate(AuthenticateRequest model)
+        //{
+        //    var response = _userService.Authenticate(model);
+
+        //    Console.WriteLine("Hello World!");
+
+        //    if (response == null)
+        //        return BadRequest(new { message = "Username or password is incorrect" });
+
+        //    return Ok(response);
+        //}
+
+        ////[Authorize]
+        //[HttpGet]
+        //public IActionResult GetAll()
+        //{
+        //    var users = _userService.GetAll();
+        //    return Ok(users);
+        //}
+
+        //[HttpGet("persons")]
+        //public IActionResult GetAllPerson()
+        //{
+        //    //var users = _userService.GetAll();
+        //    var person = context.Persons.ToList();
+        //    return Ok(person);
+        //}
+
+        //[HttpPost("persons/getPersonById")]
+        //public IActionResult GetPersonById(getPersonByIdParameterModel model)
+        //{
+        //    //var users = _userService.GetAll();
+        //    var person = context.Persons.FirstOrDefault(f => f.Id == model.id);
+        //    return Ok(person);
+        //}
+    }
+
+    public class getPersonByIdParameterModel
+    {
+        public int id { get; set; }
+    }
+}

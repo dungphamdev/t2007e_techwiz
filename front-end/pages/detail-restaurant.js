9b1ec2cca41c919510953b2ba5c640b92639f7ec
hoangdada11119992@gmail.com <hoangdada11119992@gmail.com>
2021-04-26 18:37:55 +0700

    add detail + faq

--- front-end/pages/detail-restaurant.js
+++ front-end/pages/detail-restaurant.js
@@ -16,7 +16,7 @@ function myFunction() {
 $("#decreaseBtn-1").click(function () {
   let val = $("#quantityInOrder").val();
   if (val > 1) {
-    $("#quantityInOrder").val(val - 1);
+    $("#quantityInOrder").val(val - 1); 
   }
 });
 $("#increaseBtn-1").click(function () {
@@ -29,4 +29,18 @@ $("#sumPriceInOrder").text(pricetxt);
 $(".adjustAmountBtn").click(function () {
   let sumPrice = price * $("#quantityInOrder").val();
   $("#sumPriceInOrder").text("$" + sumPrice);
+});
+
+$(".productInBasket").each(function(){
+  $(this).find(".decreaseBtn-2").click(function(){
+      let quantity = $(this).parent().find(".quantityInBasket").val();
+      if (quantity > 1) {
+          $(this).parent().find(".quantityInBasket").val(quantity - 1);
+      }
+  });
+  $(this).find(".increaseBtn-2").click(function(){
+      let quantity = $(this).parent().find(".quantityInBasket").val();
+      $(this).parent().find(".quantityInBasket").val(parseInt(quantity) + 1);
+      console.log(parseInt(quantity) + 1);
+  });
 });
